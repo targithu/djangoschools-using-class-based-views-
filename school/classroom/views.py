9ff864d006bf6db768b79ba93b205d5b1405ecf8
Tarun S Hegde <101446457+targithu@users.noreply.github.com>
2022-07-28 14:11:00 +0530

    Update views.py

--- school/classroom/views.py
+++ school/classroom/views.py
@@ -1,9 +1,9 @@
 from dataclasses import field
 from django.shortcuts import render
 from django.urls import reverse,reverse_lazy
-from django.views.generic import TemplateView,FormView,CreateView,ListView,DetailView,UpdateView,DeleteView
+from django.views.generic import TemplateView,FormView,CreateView,ListView,DetailView,UpdateView,DeleteView 
 from classroom.models import Teacher
-from classroom.forms import ContactForm
+from classroom.forms import ContactForm #have to import from forms
 # Create your views here.
 class HomeView(TemplateView):
     template_name='classroom/home.html'
@@ -18,19 +18,20 @@ class ContactFormView(FormView):
     success_url="/classroom/thank_you/"
     #what to do with the form
     def form_valid(self,form):
-      print(form.cleaned_data)
-      return super().form_valid(form)
+      print(form.cleaned_data)# return a dictionary of form
+      return super().form_valid(form)# similar to formname(request.POST)
 
 class TeacherCreateView(CreateView):
-    model=Teacher
+    model=Teacher#to connect the model in class based views
     #model_form.html
     # .save()
-    fields="__all__"
-    success_url=reverse_lazy('classroom:list_teacher')
+    fields="__all__"#include all fields from the model
+    success_url=reverse_lazy('classroom:list_teacher')#it does the same as reverse does in function based views
 
 class TeacherListView(ListView):
     #model_list.html
     model=Teacher
+    #By default here QuerySet=Teacher.objects.all() can change if u want to
     context_object_name="teacher_list"
 class TeacherDetailView(DetailView):
     #RETURN ONLY ONE MODEL ENTRY PK
