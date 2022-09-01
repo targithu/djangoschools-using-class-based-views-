2f3bbbddacf8d69909e4b4b6d179acc73800398a
Tarun S Hegde <101446457+targithu@users.noreply.github.com>
2022-09-01 13:48:43 +0530

    Update models.py

--- school/classroom/models.py
+++ school/classroom/models.py
@@ -5,7 +5,7 @@ class Teacher(models.Model):
     first_name=models.CharField(max_length=30)
     last_name=models.CharField(max_length=30)
     subject=models.CharField(max_length=30)
-
+   
     def __str__(self):
         return f"{self.first_name} {self.last_name} teaches {self.subject} subject."
     
