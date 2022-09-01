04c48ec361a9dcc0db7da361ffabe1491f926d71
Tarun S Hegde <101446457+targithu@users.noreply.github.com>
2022-09-01 13:49:05 +0530

    Update forms.py

--- school/classroom/forms.py
+++ school/classroom/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class ContactForm(forms.Form):
     name=forms.CharField()
-    message=forms.CharField(widget=forms.Textarea)
+    message=forms.CharField(widget=forms.Textarea)  
